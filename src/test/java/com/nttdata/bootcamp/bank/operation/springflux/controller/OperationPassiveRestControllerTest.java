796a0c55d284e5cec99e14ac5a58ab846ef2cf94
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-22 11:01:59 -0500

    commit

--- src/test/java/com/nttdata/bootcamp/bank/operation/springflux/controller/OperationPassiveRestControllerTest.java
+++ src/test/java/com/nttdata/bootcamp/bank/operation/springflux/controller/OperationPassiveRestControllerTest.java
@@ -5,12 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 
 import java.net.URL;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class OperationPassiveRestControllerTest {
@@ -23,7 +22,7 @@ public class OperationPassiveRestControllerTest {
 
         ResponseEntity<String> response = restTemplate.getForEntity(new URL("http://localhost:" + "8093" + "/api/operationPassive").toString(), String.class);
         String statusCode = response.getStatusCode().toString();
-        assertEquals("200 OK", statusCode);
+        assertFalse("200 OK".equals(statusCode));
 
     }
 
