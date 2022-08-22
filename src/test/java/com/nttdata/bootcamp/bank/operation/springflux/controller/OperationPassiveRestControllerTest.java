5d7ee56e882bdd6d87efe0ccdf6e09678fcd9872
ocandelc <oscarerick.candelacarbajal@nttdata.com>
2022-08-22 11:23:11 -0500

    commit 2002.08.22 11.24.01

--- src/test/java/com/nttdata/bootcamp/bank/operation/springflux/controller/OperationPassiveRestControllerTest.java
+++ src/test/java/com/nttdata/bootcamp/bank/operation/springflux/controller/OperationPassiveRestControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.net.URL;
 
@@ -17,12 +18,24 @@ public class OperationPassiveRestControllerTest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
     public void readAllTest() throws Exception {
 
+        /*
         ResponseEntity<String> response = restTemplate.getForEntity(new URL("http://localhost:" + "8093" + "/api/operationPassive").toString(), String.class);
         String statusCode = response.getStatusCode().toString();
         assertFalse(!("200 OK").equals(statusCode));
+        */
+
+        webTestClient.get()
+                .uri("/api/operationPassive")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .consumeWith(response ->  assertFalse(("200 OK").equals(response.getStatus().toString())));
 
     }
 
